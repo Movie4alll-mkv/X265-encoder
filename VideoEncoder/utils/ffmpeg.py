b653381e23f2b1b0c0524c922d450d15cbb8f6b7
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2022-10-12 17:20:41 +0500

    Update ffmpeg.py

--- VideoEncoder/utils/ffmpeg.py
+++ VideoEncoder/utils/ffmpeg.py
@@ -93,7 +93,7 @@ async def encode(filepath):
             audio_opts += ' -c:a copy'
 
     # Resolution
-     r == '720':
+     resolution == '720':
         resolution = '-vf scale=1280'
     
     finish = '-threads 8'
